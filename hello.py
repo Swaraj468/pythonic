4079cdadf2f206fd3c701a671f304ce75e3aaf2b
Swaraj <swarajbandhi0000@gmail.com>
2025-09-06 13:10:31 +0530

    doing python in pythonic way

---
+++ hello.py
@@ -0,0 +1,11 @@
+# message = "Hello python"
+
+# print(message)
+
+# message = "Hello Python!"
+# print(message)
+message = {}
+
+mesage = "Hello Python Crash Course reader!"
+print(mesage)
+print(message)
