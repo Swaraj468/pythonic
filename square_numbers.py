f0f95f8d47d7703532d53a1c21dd98c58a3b5cd8
Swaraj <swarajbandhi0000@gmail.com>
2025-09-16 00:52:12 +0530

    don till simple statisctis with a list numbers and read till three easy ass functions which are min(), max() and sum() functions, tomorrow we will continue from list comprehensions

---
+++ square_numbers.py
@@ -0,0 +1,9 @@
+squares = []
+for values in range(1, 11):
+    square = values ** 2;
+    squares.append(square)
+print(squares)
+#  or we can write more concisely 
+# squares.append(value ** 2) instead of making a new variable and appending that variable to squares
+#  we can use min(), max(), sum() for finding minimum number in a list, for maximum we have max function
+#  for sum of the list we have sum function
